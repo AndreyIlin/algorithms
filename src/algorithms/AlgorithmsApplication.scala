1a3dff74ed98c1e9be92470950d6082d2c5c1268
ailin <ilinandreymail@gmail.com>
2016-04-04 23:41:30 +0300

    Karatusba algorithm. Failed on big numbers. See align.

--- src/algorithms/AlgorithmsApplication.scala
+++ src/algorithms/AlgorithmsApplication.scala
@@ -1,7 +1,9 @@
 package algorithms
 
+import algorithms.multiplication.KaratsubaAlgorithm
+
 object AlgorithmsApplication {
   def main(args: Array[String]) {
-
+    println(KaratsubaAlgorithm.multiply("49823261", "44269423"))
   }
 }
--- src/algorithms/multiplication/KaratsubaAlgorithm.scala
+++ src/algorithms/multiplication/KaratsubaAlgorithm.scala
@@ -3,44 +3,60 @@ package algorithms.multiplication
 /**
   * Fast multiplication algorithm
   */
-class KaratsubaAlgorithm {
+object KaratsubaAlgorithm {
 
   /**
     * Multiplies two given numbers
     * Numbers can have length more than standard `Int.MaxValue`
     * That's why they have String type
+    *
     * @param x first multiplier
     * @param y second multiplier
     * @return result of multiplication
     */
   def multiply(x: String, y: String): String = {
-    if (isInt(x).isEmpty) {
-      devide(x)
-    }
-    ""
+    val first = align(x)
+    val second = align(y)
+    val n = first.length
+
+    val ab = divide(first)
+    val cd = divide(second)
+
+    // 10^n(ac) + 10^n/2((a+b)(c+d)-ac-bd) + bd
+    val ac = (ab._1, cd._1)
+    val acResult = getOrCall(ac._1, ac._2)
+    val bd = (ab._2, cd._2)
+    val bdResult = getOrCall(bd._1, bd._2)
+    val apb: BigInt = BigInt(ac._1) + BigInt(bd._1)
+    val cpd: BigInt = BigInt(ac._2) + BigInt(bd._2)
+    val apbcpdResult = getOrCall(apb.toString, cpd.toString)
+
+    (BigInt(10).pow(n) * BigInt(acResult) +
+      BigInt(10).pow(n / 2) * (BigInt(apbcpdResult) - BigInt(acResult) - BigInt(bdResult)) +
+      BigInt(bdResult)).toString
   }
 
-  /**
-    * Divides given number on two parts.
-    * The first one tens
-    * The second one units
-    * @param x number to divide
-    */
-  private def devide(x: String): Unit = {
-
-  }
-
-  private def isInt(x: String): Option[Int] = {
-    val length = x.length
-    val isLonger = length > Int.MaxValue.toString.length
-    if (isLonger) {
-      None
+  private def align(number: String): String = {
+    if (number.length % 2 == 0) {
+      number
     } else {
-      try {
-        Some(x.toInt)
-      } catch {
-        case NumberFormatException => None
-      }
+      "0" + number
     }
+  }
+
+  private def getOrCall(multipliers: (String, String)): String = {
+    if (multipliers._1.length > 1 || multipliers._2.length > 1) {
+      multiply(multipliers._1, multipliers._2)
+    } else {
+      (multipliers._1.toInt * multipliers._2.toInt).toString
+    }
+  }
+
+  private def divide(x: String): (String, String) = {
+    val idx = x.length / 2
+    val a = x.substring(0, idx)
+    val b = x.substring(idx, x.length)
+
+    (a, b)
   }
 }
