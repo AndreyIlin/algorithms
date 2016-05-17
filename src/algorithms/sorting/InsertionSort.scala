1858753c449e1d918119bf456aec46d9d7f039c1
Andrii Ilin <Andrii.Ilin@teamaol.com>
2016-05-17 17:54:52 +0300

    Insertion sorting: added small doc to describe algorithm

--- src/algorithms/sorting/InsertionSort.scala
+++ src/algorithms/sorting/InsertionSort.scala
@@ -1,5 +1,13 @@
 package algorithms.sorting
 
+/**
+  * Insertion sort algorithm
+  * Complexity is O(sqr(n))
+  * Can be useful on small amounts of data
+  * Doesn't use additional memory, except one field to store key value
+  * Modifies given array
+  * The left part of array divided by index of the first loop is always sorted before each iteration
+  */
 object InsertionSort {
   def sort(toSort: Array[Int]): Array[Int] = {
     for (i <- 1 until toSort.length) {
