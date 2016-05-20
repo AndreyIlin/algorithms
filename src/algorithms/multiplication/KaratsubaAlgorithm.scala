0f7b0f8658a22434048b276738fc364030d6ddc4
Andrii Ilin <Andrii.Ilin@teamaol.com>
2016-05-20 14:46:13 +0300

    Karatsuba algorithm edited
    * Rewrote code of algorithm in functional style
    * Added test

--- src/algorithms/multiplication/KaratsubaAlgorithm.scala
+++ src/algorithms/multiplication/KaratsubaAlgorithm.scala
@@ -1,5 +1,6 @@
 package algorithms.multiplication
 
+import scala.annotation.tailrec
 import scala.math.BigInt
 
 /**
@@ -11,60 +12,56 @@ object KaratsubaAlgorithm {
   /**
     * Multiplies two given long numbers
     *
-    * @param x first multiplier
-    * @param y second multiplier
+    * @param a first multiplier
+    * @param b second multiplier
     * @return result of multiplication
     */
-  def multiply(x: String, y: String): String = {
-    var oneTwo = ("", "")
-    if (x.length > y.length) {
-      oneTwo = align(x, y)
-    } else {
-      oneTwo = align(y, x)
-    }
-    val first = oneTwo._1
-    val second = oneTwo._2
-    val n = first.length
+  def multiply(a: String, b: String): String = {
+    val tuple = align(a, b)
 
-    val ab = divide(first)
-    val cd = divide(second)
+    val ab = divide(tuple._1)
+    val cd = divide(tuple._2)
+    val n = tuple._1.length
 
     // 10^n(ac) + 10^n/2((a+b)(c+d)-ac-bd) + bd
-    val ac = (ab._1, cd._1)
-    val acResult = getOrCall(ac._1, ac._2)
-    val bd = (ab._2, cd._2)
-    val bdResult = getOrCall(bd._1, bd._2)
-    val apb: BigInt = BigInt(ac._1) + BigInt(bd._1)
-    val cpd: BigInt = BigInt(ac._2) + BigInt(bd._2)
-    val apbcpdResult = getOrCall(apb.toString, cpd.toString)
-    val numb = BigInt(apbcpdResult) - BigInt(acResult) - BigInt(bdResult)
-
-    (BigInt(10).pow(n) * BigInt(acResult) +
-      BigInt(10).pow(n / 2) * numb +
-      BigInt(bdResult)).toString
+    val axc = BigInt(evaluate(ab._1, cd._1))
+    val bxd = BigInt(evaluate(ab._2, cd._2))
+    val apb = BigInt(ab._1) + BigInt(ab._2)
+    val cpd = BigInt(cd._1) + BigInt(cd._2)
+    (BigInt(10).pow(n) * axc + BigInt(10).pow(n / 2) * (BigInt(evaluate(apb.toString, cpd.toString)) - axc - bxd) + bxd).toString
   }
 
-  private def align(one: String, two: String): (String, String) = {
-    val o = alignOne(one)
-    val diff = o.length - two.length
-    var a = diff
-    var t = two
-    while(a > 0) {
-      t = "0" + t
-      a -= 1
-    }
-    (o, t)
+  private def align(a: String, b: String): (String, String) = {
+    even(normalize(a, b))
   }
 
-  private def alignOne(number: String): String = {
-    if (number.length % 2 != 0) {
-      "0" + number
+  private def even(t: (String, String)): (String, String) = {
+    if (t._1.length % 2 == 0) {
+      t
     } else {
-      number
+      ("0" + t._1, "0" + t._2)
     }
   }
 
-  private def getOrCall(multipliers: (String, String)): String = {
+  private def normalize(a: String, b: String): (String, String) = {
+    @tailrec
+    def normalize(s: String, l: Int): String = {
+      if (l > 0) {
+        normalize("0" + s, l - 1)
+      } else {
+        s
+      }
+    }
+
+    val diff = a.length - b.length
+    if (diff > 0) {
+      (a, normalize(b, diff))
+    } else {
+      (normalize(a, -diff), b)
+    }
+  }
+
+  private def evaluate(multipliers: (String, String)): String = {
     val a = BigInt(multipliers._1).toString()
     val b = BigInt(multipliers._2).toString()
     if (a.length > 1 || b.length > 1) {
